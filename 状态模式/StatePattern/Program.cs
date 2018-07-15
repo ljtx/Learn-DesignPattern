e631dda3653e89e7e57e5b0eac599450bdc52eb3
ambiel_li <864305381@qq.com>
2018-07-15 15:17:24 +0800

    第二次提交

---
+++ 状态模式/StatePattern/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace StatePattern
+{
+     public sealed class Order
+     {
+        private State current;
+
+        public Order()
+        {
+            current = new WaitForAcceptance();
+            IsCancel = false;
+        }
+        public double Minute {get;set;}
+        public bool IsCancel { get; set; }
+        public bool TaskFinished{get;set;}
+       
+        public void SetState(State s)
+        {
+            current = s;
+        }
+        public void Action()
+        {
+            current.Process(this);
+        }
+    }
+    public interface State
+    {
+        void Process(Order order);
+    }
+    public class WaitForAcceptance:State
+    {
+        public void Process(Order order)
+        {
+            Console.WriteLine("订单开始受理！");
+            if (order.Minute < 30 && order.IsCancel)
+            {
+                Console.WriteLine("您有半个小时的时间可以取消订单！");
+                order.SetState(new CancelOrder());
+                order.Action();
+            }
+            order.SetState(new AcceptAndDeliver());
+            order.TaskFinished = false;
+            order.Action();
+        }
+    }
+     public sealed class AcceptAndDeliver : State
+    {
+        public void Process(Order order)
+        {
+            if (order.Minute < 30 && order.IsCancel)
+            {
+                Console.WriteLine("您有半个小时的时间可以取消订单！");
+                order.SetState(new CancelOrder());
+                order.Action();
+            }
+            if (order.TaskFinished==false)
+            {
+                Console.WriteLine("我们货物已经准备好，可以发货了，不可以撤销订单！");
+                order.SetState(new ConfirmationReceipt());
+                order.Action();
+            }
+        }
+    }
+    public sealed class ConfirmationReceipt : State
+    {
+        public void Process(Order order)
+        {
+            Console.WriteLine("检查货物，没问题可以就可以签收！");
+            order.SetState(new Success());
+            order.TaskFinished = false;
+            order.Action();
+        }
+    }
+
+       
+    public sealed class Success : State
+    {
+        public void Process(Order order)
+        {
+            Console.WriteLine("订单已经完成");
+            order.TaskFinished = true;
+        }
+    }
+
+       
+    public sealed class CancelOrder : State
+    {
+        public void Process(Order order)
+        {
+            Console.WriteLine("检查货物，有问题，取消订单！");
+            order.TaskFinished = true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            //可以取消订单
+           // order.IsCancel = true;
+            order.Minute = 20;
+            order.Action();
+           
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 迭代器模式/IteratorPattern/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace IteratorPattern
+{
+    /// <summary>
+    /// 抽象聚合类
+    /// </summary>
+    public interface IAggregate
+    {
+        Iterator GetIterator();
+    }
+ 
+    // 迭代器抽象类
+    public interface Iterator
+    {
+        bool MoveNext();
+        Object GetCurrent();
+        void Next();
+        void Reset();
+    }
+    public sealed class ConcreteAggregate:IAggregate
+    {
+        private string[] collection;
+
+        public ConcreteAggregate()
+        {
+            collection = new string[] { "白羊","金牛","双子","巨蟹" };
+        }
+ 
+        public Iterator GetIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+ 
+        public int Length
+        {
+            get { return collection.Length; }
+        }
+ 
+        public string GetElement(int index)
+        {
+            return collection[index];
+        }
+    }
+  // 具体迭代器类
+    public sealed class ConcreteIterator : Iterator
+    { 
+        private ConcreteAggregate _list;
+        private int _index;
+ 
+        public ConcreteIterator(ConcreteAggregate list)
+        {
+            _list = list;
+            _index = 0;
+        }
+ 
+        public bool MoveNext()
+        {
+            if (_index < _list.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+ 
+        public Object GetCurrent()
+        {
+            return _list.GetElement(_index);
+        }
+ 
+        public void Reset()
+        {
+            _index = 0;
+        }
+ 
+        public void Next()
+        {
+            if (_index < _list.Length)
+            {
+                _index++;
+            }
+ 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Iterator iterator;
+            IAggregate list = new ConcreteAggregate();
+            iterator = list.GetIterator();
+ 
+            while (iterator.MoveNext())
+            {
+                string ren = (string)iterator.GetCurrent();
+                Console.WriteLine(ren);
+                iterator.Next();
+            }
+ 
+            Console.ReadKey();
+        }
+    }
+}
